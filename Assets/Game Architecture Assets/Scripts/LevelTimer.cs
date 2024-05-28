69fb8924dce52fc2b5d9ab28243d1a477dd3eb3d
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-28 00:42:55 -0700

    Completed Game Over scene

--- Assets/Game Architecture Assets/Scripts/LevelTimer.cs
+++ Assets/Game Architecture Assets/Scripts/LevelTimer.cs
@@ -1,16 +1,19 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class LevelTimer : MonoBehaviour
 {
     [SerializeField] private float totalTime = 30.0f;
+    [SerializeField] private TextMeshProUGUI levelTimerText;
     private float currentTime;
     public UnityEvent OnTimerEnd;
 
     public void StartTimer()
     {
         currentTime = totalTime;
+        levelTimerText.text = currentTime.ToString();
         StartCoroutine(StartCountdown());
     }
 
@@ -20,6 +23,7 @@ public class LevelTimer : MonoBehaviour
         {
             yield return new WaitForSeconds(1.0f);
             currentTime--;
+            levelTimerText.text = currentTime.ToString();
         }
         OnTimerEnd?.Invoke();
     }
